0a783f0f793d628d657cff36f7718861cd6f4a40
vishalgarg01 <vishalgarghi@gmail.com>
2021-01-29 11:44:43 +0530

    REST with mongo and express

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
+const Dishes = require('./models/dishes');
+const mongoose=require('mongoose');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 var app = express();
 
@@ -21,6 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +55,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//server started in bin/www
