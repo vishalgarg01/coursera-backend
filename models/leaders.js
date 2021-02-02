3e8cde59f1b371262926756f79cd19aef959c2a6
vishalgarg01 <vishalgarghi@gmail.com>
2021-02-02 13:54:57 +0530

    authentication cookies and session

---
+++ models/leaders.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const leadersSchema =new Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    designation:{
+        type:String,
+        required:true
+    },
+    abbr:{
+        type:String,
+        required:true
+    },
+    description: {
+        type: String,
+        required:true      
+    },
+},  {
+        timestamp:true
+    }
+);
+var leaders=mongoose.model('Leader',leadersSchema);
+module.exports = leaders;
